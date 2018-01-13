6b5e97a5e131ec3e0b11bcd55c2ee7c54de9d6c0
Mike Pekala <mpekala411@gmail.com>
2018-01-13 17:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ function shout(string){
   return string.toUpperCase()
 };
 
-shout("hello");
 
 function logShout(string){
-  return console.log(string.toUpperCase())
+  console.log(string.toUpperCase())
 }
 
-logShout("hello")
 
 function whisper(string){
   return string.toLowerCase();
 }
 
-whisper("HELLO")
-
 function logWhisper(string){
-  return console.log(string.toLowerCase())
+  console.log(string.toLowerCase())
 }
-
-logWhisper("HELLO")
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()) return "I can't hear you!";
